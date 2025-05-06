9784dad20adfb23432a1dcd932ad609f52ab1879
Dev-Iron <2soonchoel@gmail.com>
2025-05-07 00:27:49 +0900

    프로젝트 초기 세팅 (패키지 구조, build.gradle, yml 구성

---
+++ src/main/java/com/dev/crud_project_01/CrudProject01Application.java
@@ -0,0 +1,13 @@
+package com.dev.crud_project_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudProject01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudProject01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/crud_project_01/CrudProject01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.crud_project_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudProject01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
